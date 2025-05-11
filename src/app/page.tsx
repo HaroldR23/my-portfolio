02297f5a49c81f5665cf483160e0ed928eb4375a
HaroldR23 <ahsro99@gmail.com>
2025-05-11 19:26:22 -0300

    feat: implement AboutME component section, just structure and mock texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import AboutMe from "./components/AboutMe/AboutMe";
+
 const HomePage = () => {
-    return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is the home page of our application.</p>
-        </div>
-    );
+  return (
+    <div>
+      <AboutMe />
+    </div>
+  );
 };
 
 export default HomePage;
-// This is a simple home page component that displays a welcome message and a brief description of the application
