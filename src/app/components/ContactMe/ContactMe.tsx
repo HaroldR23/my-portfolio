1a372210612bfb0994974e68be921016cbef05a5
HaroldR23 <ahsro99@gmail.com>
2025-05-12 17:59:41 -0300

    feat: implement Blog and ContactMe components, without styles, just structure

---
+++ src/app/components/ContactMe/ContactMe.tsx
@@ -0,0 +1,28 @@
+const ContactMe = () => {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>If you want to contact me, please send me an email at:</p>
+      <span>ahsro99@gmail.com</span>
+      <p>Or you can reach me on my social media:</p>
+      <ul>
+        <li>
+          <a 
+            href="https://www.linkedin.com/in/haroldrodriguez/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://github/HaroldR23.com" target="_blank" rel="noopener noreferrer">
+                GitHub
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+};
+
+export default ContactMe;
