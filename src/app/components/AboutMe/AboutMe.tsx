80a8ab2cef0e1dc5286a41dca9c1ada8895478cc
HaroldR23 <ahsro99@gmail.com>
2025-05-19 14:06:37 -0300

    refactor: modify AboutMe component styles to become it more responsive

--- src/app/components/AboutMe/AboutMe.tsx
+++ src/app/components/AboutMe/AboutMe.tsx
@@ -16,13 +16,22 @@ import {
   TEXT_CONTAINER 
 } from "@/app/constants/styles/aboutMeStyles";
 import Image from "next/image";
+import { useMediaQuery } from "react-responsive";
 
 const AboutMe = () => {
+  const isMobile = useMediaQuery( {maxWidth: 767} );
+
   return (
     <div id="aboutMe" className={ABOUT_ME_CONTAINER}>
+      {isMobile && <Image
+        alt="public/hr_picture.png" 
+        src="/hr_picture.png" className="rounded-full" width={250} height={250}
+      />}
       <div className={TEXT_CONTAINER}>
         <h1 className={INTRODUCTION_MY_SELF_STYLE}>{INTRODUCTION_MY_SELF_ENG}</h1>
-        <h3 className="text-xl font-bold tracking-tight">{INTRODUCTION_MY_SELF_ENG_SUB_HEADER}</h3>
+        <h3 className="text-base sm:text-lg md:text-lg lg:text-xl font-bold tracking-tight">
+          {INTRODUCTION_MY_SELF_ENG_SUB_HEADER}
+        </h3>
         <p className={PARAGRAPH_STYLE}>
           {ABOUT_ME_TEXT_ENG}
         </p>
@@ -34,9 +43,10 @@ const AboutMe = () => {
           {CV_BUTTON_TEXT_ENG}
         </a>
       </div>
-      <Image 
+      {!isMobile && <Image
         alt="public/hr_picture.png" 
-        src="/hr_picture.png" className="rounded-full" width={400} height={400}/>
+        src="/hr_picture.png" className="rounded-full" width={400} height={400}
+      />}
     </div>
   );
 };
--- src/app/constants/styles/aboutMeStyles.ts
+++ src/app/constants/styles/aboutMeStyles.ts
@@ -1,11 +1,14 @@
 import { getDownloadButtonStyle } from "@/app/utils/utils";
 
-export const INTRODUCTION_MY_SELF_STYLE = "text-5xl font-bold tracking-tight m-0";
+export const INTRODUCTION_MY_SELF_STYLE = `
+text-xl sm:text-2xl md:text-3xl lg:text-5xl font-bold tracking-tight m-0`;
+
 export const ABOUT_ME_CONTAINER = `
     max-w-7xl mx-auto px-4 sm:px-6 lg:px-8
     py-16 flex flex-col md:flex-row
     items-center justify-between gap-12
 `;
 export const TEXT_CONTAINER = "flex-1 space-y-12";
-export const PARAGRAPH_STYLE = "text-lg text-gray-300 leading-relaxed max-w-2xl";
+export const PARAGRAPH_STYLE = `
+text-base md:text-lg lg:text-xl text-gray-300 leading-relaxed max-w-2xl`;
 export const BUTTON_STYLE = getDownloadButtonStyle();
