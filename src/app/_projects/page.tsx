2c1666bac0dcccf0d3c2acbcd1794124d24b24b9
HaroldR23 <ahsro99@gmail.com>
2025-05-29 13:55:57 -0300

    fix: modify Project list importation

--- src/app/_projects/page.tsx
+++ src/app/_projects/page.tsx
@@ -1,11 +1,11 @@
 import ProjectCard from "../components/ProjectCard/ProjectCard";
-import { PROJECTS_LIST } from "@/app/constants/data/english";
+import { PROJECTS_LIST_ENG } from "../constants/data/english";
 
 const ProjectsPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       {
-        PROJECTS_LIST.map((project) => (
+        PROJECTS_LIST_ENG.map((project) => (
           <ProjectCard 
             description={project.description} 
             id={project.id} name={project.name} 
